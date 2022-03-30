c7eaf43bcd4122f077ebecb30d46aeeb4b64b1f9
Tanveer Alam <tanveer.alam@hyperdatacomputing.com>
2022-03-30 16:59:50 +0500

    React-Search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./app.css";
+import { Users } from "./users";
+import Table from "./Table.jsx";
 
+//////////////////////BASIC SEARCH
+
+// function App() {
+//   const [query, setQuery] = useState("");
+//   return (
+//     <div className="app">
+//       <input
+//         className="search"
+//         placeholder="Search..."
+//         onChange={(e) => setQuery(e.target.value.toLowerCase())}
+//       />
+//       <ul className="list">
+//         {Users.filter((asd) =>
+//           asd.first_name.toLowerCase().includes(query)
+//         ).map((user) => (
+//           <li className="listItem" key={user.id}>
+//             {user.first_name}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+////////////////////// SEARCH ON A DATATABLE
 function App() {
+  const [query, setQuery] = useState("");
+  const keys = ["first_name", "last_name", "email"];
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <input
+        className="search"
+        placeholder="Search..."
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
+      />
+      {<Table data={search(Users)} />}
     </div>
   );
 }
